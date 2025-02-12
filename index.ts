189a6615ea4124cc2b4d673915aa9ba108db106a
Alexander <alex.bukin@hotmail.com>
2025-02-12 21:41:26 +0000

    Refactor of addNote function and removal of unnecessary code

--- index.ts
+++ index.ts
@@ -6,9 +6,10 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const data = {
-  name: 'lxndrbukin',
-  age: 29,
+const askQuestion = (query: string) => {
+  return new Promise((resolve) => {
+    rl.question(query, resolve);
+  });
 };
 
 function loadNotes() {
@@ -23,33 +24,25 @@ function saveNotes(notes: any) {
   writeFileSync('notes.json', JSON.stringify(notes, null, 2));
 }
 
-function addNote() {
+async function addNote() {
   const notes = loadNotes();
-  let note = {};
-  rl.question('Enter your note: ', (noteContent: any) => {
-    note = {
-      id: notes.length + 1,
-      content: noteContent,
-      ...note,
-    };
-    rl.question('Enter tags (use comma to separate): ', (tags: any) => {
-      note = {
-        tags: tags.split(','),
-        ...note,
-      };
-      notes.push(note);
-      console.log('Note added successfully!');
-      saveNotes(notes);
-      rl.close();
-    });
-  });
+
+  const noteContent = (await askQuestion('Enter note: ')) as string;
+  const noteTags = (await askQuestion(
+    'Enter tags (comma separated): '
+  )) as string;
+
+  const note = {
+    id: notes.length + 1,
+    content: noteContent.trim(),
+    tags: noteTags.split(',').map((tag: string) => tag.trim()),
+  };
+
+  notes.push(note);
+  saveNotes(notes);
+  console.log('Note added successfully!');
+  rl.close();
 }
 
-// function showMenu() {
-//   rl.question('What is hotter? ', (answer: any) => {
-//     addNote();
-//   });
-// }
-
-// writeFileSync('index.json', JSON.stringify(data));
+loadNotes();
 addNote();
