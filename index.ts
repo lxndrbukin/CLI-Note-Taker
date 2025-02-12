296809358f898dce5c339cf78402c0d1660de010
Alexander <alex.bukin@hotmail.com>
2025-02-12 22:16:55 +0000

    Add note type interface, add showNotes function to list all notes, add showMenu function to prompt menu for possible commands and their execution

--- index.ts
+++ index.ts
@@ -1,18 +1,25 @@
 const { writeFileSync, readFileSync, existsSync } = require('fs');
 const readline = require('readline');
 
+type Note = {
+  id: number;
+  content: string;
+  tags: string[];
+  createdAt: string;
+};
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const askQuestion = (query: string) => {
+const askQuestion = (query: string): Promise<string> => {
   return new Promise((resolve) => {
     rl.question(query, resolve);
   });
 };
 
-function loadNotes() {
+function loadNotes(): Note[] {
   if (existsSync('./notes.json')) {
     const data = readFileSync('./notes.json', 'utf-8');
     return JSON.parse(data);
@@ -20,11 +27,23 @@ function loadNotes() {
   return [];
 }
 
-function saveNotes(notes: any) {
+function showNotes(): void {
+  const notes = loadNotes();
+  console.log('Notes:');
+  notes.forEach((note: Note) => {
+    console.log('\x1b[33m━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m'); // Yellow separator
+    console.log(`\x1b[1mNote #${note.id}\x1b[0m`); // Bold note title
+    console.log(`\x1b[37m${note.content}\x1b[0m`); // White note content
+    console.log(`\x1b[35mTags: \x1b[32m${note.tags.join(', ')}\x1b[0m`); // Magenta label + Green tags
+    console.log('\x1b[33m━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m\n'); // Yellow separator
+  });
+}
+
+function saveNotes(notes: Note[]): void {
   writeFileSync('notes.json', JSON.stringify(notes, null, 2));
 }
 
-async function addNote() {
+async function addNote(): Promise<void> {
   const notes = loadNotes();
 
   const noteContent = (await askQuestion('Enter note: ')) as string;
@@ -36,6 +55,7 @@ async function addNote() {
     id: notes.length + 1,
     content: noteContent.trim(),
     tags: noteTags.split(',').map((tag: string) => tag.trim()),
+    createdAt: new Date().toISOString(),
   };
 
   notes.push(note);
@@ -44,5 +64,24 @@ async function addNote() {
   rl.close();
 }
 
-loadNotes();
-addNote();
+async function showMenu(): Promise<void> {
+  const command = (await askQuestion(
+    'Enter a command (add, list, quit): '
+  )) as string;
+
+  switch (command) {
+    case 'add':
+      addNote();
+      break;
+    case 'list':
+      showNotes();
+      break;
+    case 'quit':
+      rl.close();
+      break;
+    default:
+      console.log('Invalid command');
+  }
+}
+
+showMenu();
